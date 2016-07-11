19236cf34376617739ab44f04d019b8df17e0285
nursetta <n_ursetta@yahoo.com>
2016-07-11 09:20:55 -0600

    final homeowrk 2

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
+                  process.env.MONGODB_URI || 
                   "mongodb://localhost/personal-api");
 
 module.exports.Movie= require("./movie.js");
